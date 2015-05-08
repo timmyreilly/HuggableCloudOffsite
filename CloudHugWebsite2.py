c1a736b79ae89d64caa1f915dadb470caca9cc8f
timmyreilly <timreilly@live.com>
2015-05-08 12:42:00 -0700

    When queue has messages we can pop

--- CloudHugWebsite2.py
+++ CloudHugWebsite2.py
@@ -6,23 +6,39 @@ from helperFUNctions import *
 from flask import Flask, render_template
 app = Flask(__name__)
 
-#from azure.storage import TableService, Entity, QueueService
 
-#myaccount = getAccount()
-#mykey = getKey()
-
-#table_service = TableService(account_name=myaccount, account_key=mykey)
-#queue_service = QueueService(account_name=myaccount, account_key=mykey)
-
-
-        
 
 
 while True:
-    x = eval(getMessage())
-    print type(x)
-    for i in sorted(x):
-        print str(i) + ' ' + str(x[i])
+    if peekMessageAvailable():
+        x = eval(getMessage())
+        for i in sorted(x):
+            print str(i) + ' ' + str(x[i])
+    else:
+        x = 'Nothing in Queue'
+
+
+
+    #x = getDictFromQueue()
+    #print x
+    #if x:
+    #    for i in sorted(x):
+    #        print str(i) + ' ' + str(x[i])
+    #else: 
+    #    print "Queue is empty"
+
+
+#while True:
+#    if getMessage():
+#        x = eval(getMessage())
+#    else:
+#        x = 'nothing to return'
+#    print type(x)
+#    for i in sorted(x):
+#        print str(i) + ' ' + str(x[i])
+
+
+
    
 #@app.route("/")
 #def home():
@@ -33,7 +49,16 @@ while True:
 #def hello():
 #    print getMessage() 
 #    return str(getMessage())
+#from azure.storage import TableService, Entity, QueueService
 
+#myaccount = getAccount()
+#mykey = getKey()
+
+#table_service = TableService(account_name=myaccount, account_key=mykey)
+#queue_service = QueueService(account_name=myaccount, account_key=mykey)
+
+
+        
 
 
 if __name__ == "__main__":
--- helperFUNctions.py
+++ helperFUNctions.py
@@ -30,17 +30,33 @@ def getQueueName():
     '''returns string of current working queue'''
     return queueName
 
+def peekMessageAvailable():
+    ''' returns a True is a messsage is available in Queue, False if empty '''
+    messages = queue_service.peek_messages(getQueueName())
+    for message in messages:
+        if message.message_text:
+            return True
+        else:
+            return False
+
 def getMessage():
+    ''' returns a unicode string object of the contents of the queue '''
     messages = queue_service.get_messages(getQueueName())
     for message in messages:
         messageText = message.message_text
         queue_service.delete_message(getQueueName(), message.message_id, message.pop_receipt)
         return messageText
 
-def getDictOfUnicode(x):
-    return eval(str(x))
+def getDictFromQueue():
+    ''' bad function doesn't do anything do not use ''' 
+    if getMessage():
+        x = eval(getMessage())
+        return x
+    else:
+        return False
 
 def generateRandom(xyorz):
+    ''' generates random numbers for x (single digit), y(2 digit, z(3 digit) '''
     if xyorz == 'x':
         return random.randint(0, 9)
     if xyorz == 'y':
