bb2dca35dfa1a7eab806ffecdf1deea6a0e4fa56
timmyreilly <timreilly@live.com>
2015-05-21 12:51:03 -0700

    finalized pi version

--- raspberryHugs.py
+++ raspberryHugs.py
@@ -3,10 +3,10 @@ from helperFUNctions import *
 
 from azure.storage import Entity, QueueService
 
-#import spidev CHANGE THIS BACK ON FOR PI DEPLOYMENT
+import spidev CHANGE THIS BACK ON FOR PI DEPLOYMENT
 
-#spi = spidev.SpiDev()
-#spi.open(0,0)
+spi = spidev.SpiDev()
+spi.open(0,0)
 
 myaccount = getAccount()
 mykey = getKey()
@@ -20,17 +20,17 @@ i = 0
 periods = ('a', 'b', 'c', 'd')
 
 
-#def analog_read(channel):
-#        r = spi.xfer2([1, (8 + channel) << 4, 0])
-#        adc_out = ((r[1]&3) << 8) + r[2]
-#        return adc_out
+def analog_read(channel):
+        r = spi.xfer2([1, (8 + channel) << 4, 0])
+        adc_out = ((r[1]&3) << 8) + r[2]
+        return adc_out
 
-#a_r = analog_read
+a_r = analog_read
 
 
-a_r = generateRandom
+#a_r = generateRandom
 
-a_r(0)
+#a_r(0)
 
 def get_state():
     record = {}
