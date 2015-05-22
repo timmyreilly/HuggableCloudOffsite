04828c28ed65acec3bf402d59b9a5b6883c58edd
timmyreilly <timreilly@live.com>
2015-05-22 16:49:19 -0700

    added old school refresh page for state

--- CloudWebsiteOne.py
+++ CloudWebsiteOne.py
@@ -5,3 +5,10 @@ from helperFUNctions import *
 
 from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return get_state_managed_queue()
+
+if __name__ == "__main__":
+    app.run()
