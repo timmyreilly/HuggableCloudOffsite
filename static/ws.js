5a5c260b555d095768652c8396c9fa0b4f74934f
timmyreilly <timreilly@live.com>
2015-06-01 19:42:35 -0700

    refactored ws.js

--- static/ws.js
+++ static/ws.js
@@ -1,35 +1,31 @@
 ﻿var ws,                 // websocket
     prev_data;          // remember data fetched last time
 
+
 function establish_websocket(port) {
     if ("WebSocket" in window) {
         ws = new WebSocket("ws://" + document.domain + ":" + port.toString() + "/updated");
         ws.onstart = function () {
             ws.send('started');
-        }
+        };
         ws.onmessage = function (msg) {
             load_data();
         };
         ws.onclose = function (msg) {
-            $("#updated").html('SERVER DISCONNECT');
+            $("#updated").html('SERVER DISCONNECTEDDDDD');
             $("#updated").css('backgroundColor', '#FFCCFF');
             $("#updated").fadeIn('fast');
-
         };
-
-        // load the initial data 
+        //load the initial data
         load_data();
     } else {
-        alert("WebSocket not supported");
+        alert("WEBSOCKETTT NOT SUPPORTEED");
     }
 }
 
 function load_data() {
-    // load data from /data, optionally providing a query parameter read from
-    // the #format select
-
-    var format = $('select#format option:selected')[0].value;
-    var url = '/data' + (format ? "?format=" + format : "");
+    // load data from /data
+    var url = '/data';
     $.ajax({
         url: url,
         success: function (data) {
@@ -42,44 +38,40 @@ function load_data() {
 function display_data(data) {
     // show the data acquired by load_data()
 
-    if (data && (data != prev_data)) {      // if there is data, and it's changed
+    if (data && (data != prev_data)) { // if there is data and it's changed...
 
         // compute a message to display comparing current data with previous data
         var msg, delta_msg;
         if (prev_data) {
-            console.log("data.value:", data.value, "prev_data.value:", prev_data.value);
-            var delta_value = data.value - prev_data.value;
-            delta_msg = ' (<b>' + delta_value.toFixed(3) + 's</b> since last update)';
+            console.log("data.state", data.state, "prev_data.state", prev_data.state);
+            var delta_value = "we think it's changed";
+            delta_msg = ' (<b>' + delta_value + '</b> okay...)';
         }
         else {
-            console.log("data.value:", data.value, "no prev_data");
-            console.log("data.other:", data.other, "no prev_data lalala");
+            console.log("data.state", data.state, "no prev_data");
+            console.log("data.time", data.time, "current time of request...");
             delta_msg = ' (first data point)';
         }
-        msg = "<b>" + data.value.toFixed(3) + 's</b> since ' +
-              '<a href="http://en.wikipedia.org/wiki/Unix_time">the Epoch</a>' +
-              "<b>" + data.other + "</b>";
+        msg = "<b>" + data.state + "</b> is the current cloud state at this time -> " + "<b>" + data.time "</b>";
+        // this is the message that will be sent to the client?
 
-        // update the contents of several HTML divs via jQuery
         $('div#value').html(msg + delta_msg);
-        $('div#contents').html(data.contents);
-        $('div#other').html(data.other);
+        $('div#contents').html(data.state);
+        $('div#other').html(data.time);
 
-        // remember this data, in case want to compare it to next update
+        // remember this data, in case we want to compare it to next update...
         prev_data = data;
 
-        // a little UI sparkle - show the #updated div, then after a little
-        // while, fade it away
+        // A little UI Sparkle - show the update div then after a little while fade it away...
         $("#updated").fadeIn('fast');
-        setTimeout(function () { $("#updated").fadeOut('slow'); }, 2500);
+        setTimeout(function() { $("#updated").fadeOut('slow'); }, 2500);
     }
 }
 
-$(document).ready(function () {
-    // inital document setup - hide the #updated message, and provide a
-    // "loading..." message
+$(document).ready(function() {
+    // intial document setup - hide the #updated message and provide a 'loading...' message
+
     $("div#updated").fadeOut(0);
-    $("div#contents").append("awaiting data...");
-    $("select#format").on('change', load_data);
+    $("div#contents").append("loading...");
     $("div#contents, div#value").on('click', load_data);
-});
+})
