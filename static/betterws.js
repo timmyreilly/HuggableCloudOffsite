3aed0da7058d9ab4f0de263e44fef750dab1c047
timmyreilly <timreilly@live.com>
2015-06-04 18:06:12 -0700

    added betterws.js for refactoring of javascript code

---
+++ static/betterws.js
@@ -0,0 +1,22 @@
+﻿/*
+    ws.js
+*/
+
+"use strict"
+
+function establish_websocket(port) {
+
+}
+
+function load_data() {
+    // load data from /data 
+    // use an ajax request? 
+}
+
+function display_data(data) {
+    // format at check the data for differences from load_data
+}
+
+$(document).ready(function () {
+    // initial document setup and stuff
+})
